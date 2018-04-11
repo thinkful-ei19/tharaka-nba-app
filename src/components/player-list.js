3117c7b5ed1a5353957d571229b995a1d774251e
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-04-11 10:18:20 -0700

    added more fields to player list

--- src/components/player-list.js
+++ src/components/player-list.js
@@ -4,10 +4,12 @@ export default function PlayerList(props) {
     const players = props.players.map((player, index) =>
         <li key={index}>
             <strong>{player.name}</strong> {player.pos} <br></br>
-            <span>Speed: {player.ratings[0].spd}</span>
-            
+            <span>Speed: {player.ratings[0].spd}</span> <br></br>
+            <span>Strength: {player.ratings[0].stre}</span> <br></br>
+            <span>Field Goal: {player.ratings[0].fg}%</span> <br></br>
         </li>
     );
+
     return (
         <ul className="character-list">
             {players}
